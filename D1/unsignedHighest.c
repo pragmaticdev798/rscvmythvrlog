f12bcfb0631ab789ff006ccd18675eb0bcbd4cbd
shreshth sharma <177474117+pragmaticdev798@users.noreply.github.com>
2025-05-29 21:49:56 +0530

    Add files via upload

---
+++ D1/unsignedHighest.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	unsigned long long int max = (long long) (pow(2,64)-1);
+	unsigned long long int min = (unsigned long long int) (pow(2,64)*-1);
+	printf("highest number represented by unsigned long long int is %lld\n",max);
+	printf("lowest number represented by unsigned long long int is %lld\n",min);
+        return 0;
+           } 
+
+	
+
